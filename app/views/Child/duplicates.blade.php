bb85a006349e1de77634593848e09ad7eef64107
issyzac <issyzac.iz@gmail.com>
2016-01-25 01:00:04 +0300

    child menu completed

--- app/views/Child/duplicates.blade.php
+++ app/views/Child/duplicates.blade.php
@@ -7,5 +7,60 @@
  * Time: 6:32 PM
  */ ?>
  @section('contents')
-    <span>Find Duplicates implementation</span>
+ <div class="box-row">
+     <div class="row">
+         <div class="col-lg-12 col-md-12 col-sm-12">
+
+             <div class="card" style="margin-left: 30px !important; margin-right: 30px !important;">
+
+                 <div class="card-content">
+
+                 <span class="card-title"> Find Duplicates </span>
+                 <br><br>
+                     <div class="row row-sm">
+
+                         <div class="col s2">
+                             <div class="md-form-group">
+                                 <input type="checkbox" class="md-input" ng-model="user.lastName" checked disabled>
+                                 <label>Surname</label>
+                             </div>
+                         </div>
+
+                         <div class="col s2">
+                             <p>
+                                <input type="checkbox" id="fname" />
+                                <label for="fname">First Name</label>
+                              </p>
+                         </div>
+
+                         <div class="col s2">
+                             <p>
+                                <input type="checkbox" id="dob" />
+                                <label for="dob">Date Of Birth</label>
+                              </p>
+                         </div>
+
+                         <div class="col s2">
+                             <p>
+                               <input type="checkbox" id="gender" />
+                               <label for="gender">Gender</label>
+                             </p>
+                         </div>
+
+                         <div class="col s12">
+                          <div class="col s3 pull-right">
+                               <button class="btn waves-effect waves-light pull-right" type="submit" name="action">Search
+                               </button>
+                           </div>
+                         </div>
+
+                     </div>
+
+                 </div>
+
+             </div>
+
+         </div>
+     </div>
+ </div>
  @stop
--- app/views/Child/search.blade.php
+++ app/views/Child/search.blade.php
@@ -7,5 +7,201 @@
  * Time: 6:26 PM
  */ ?>
  @section('contents')
-    <span>search child implementation</span>
+    <div class="row">
+        <div class="card" style="margin-left: 30px !important; margin-right: 30px !important;">
+            <div class="card-content">
+            <span class="card-title"> Search Filters </span>
+                <div class="row row-sm">
+                    <div class="col s6">
+                        <div class="input-field col s3">
+                          <input  id="system_id" type="text" class="validate disabled">
+                          <label class="active" for="phone">System Id</label>
+                        </div>
+
+                        <div class="input-field col s3">
+                          <input  id="id_fields" type="text" class="validate disabled">
+                          <label class="active" for="id_fields">Id Fields</label>
+                        </div>
+
+                        <div class="input-field col s3">
+                          <input  id="barcode" type="text" class="validate disabled">
+                          <label class="active" for="barcode">Barcode</label>
+                        </div>
+
+                        <div class="input-field col s3">
+                            <input  id="tempid" type="text" class="validate disabled">
+                            <label class="active" for="tempid">Temp Id</label>
+                        </div>
+
+                    </div>
+
+                   <div class="input-field col s3">
+                   <label>From</label>
+                       <input type="date" class="datepicker picker__input picker__input--active" id="dob">
+                   </div>
+
+                   <div class="input-field col s3">
+                   <label>To</label>
+                       <input type="date" class="datepicker picker__input picker__input--active" id="dob">
+                   </div>
+
+                    <div class="col-sm-12">
+                        <div class="col s3 pull-right">
+                             <button class="btn waves-effect waves-light pull-right" type="submit" name="action">Search
+                             </button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="card" style="margin-right: 30px !important; margin-left: 30px !important;">
+                <div class="card-content">
+                    <div class="card-title">
+                        <span>Children List</span>
+                    </div>
+
+                    <div class="table-responsive">
+                        <table ui-jp="dataTable" class="table table-striped b-t b-b">
+                            <thead>
+                            <tr>
+                                <th >System ID</th>
+                                <th >Firstname</th>
+                                <th >Firstname 2</th>
+                                <th >Surname</th>
+                                <th >Date Of Birth</th>
+                                <th >Gender</th>
+                                <th >Health Facility</th>
+                                <th >Place Of Birth</th>
+                                <th >Village/Domicile</th>
+                                <th >Status</th>
+                                <th >Phone</th>
+                                <th >Mother Firstname</th>
+                                <th >Mother Lastname</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td><a href="new.childinfo.html">6231721</a></td>
+                                <td>John</td>
+                                <td>S</td>
+                                <td>Doe</td>
+                                <td>12-3-2015</td>
+                                <td>Male</td>
+                                <td>Kaloleni Hospital</td>
+                                <td>Arusha</td>
+                                <td>Sanawari</td>
+                                <td>Good</td>
+                                <td>0755688675</td>
+                                <td>Miryam</td>
+                                <td>Doe</td>
+                            </tr>
+                            <tr>
+                                <td><a href="new.childinfo.html">4D39E9FB92FED199</a></td>
+                                <td>Abigael</td>
+                                <td>S</td>
+                                <td>Massam</td>
+                                <td>30-Dec-2015</td>
+                                <td>Male</td>
+                                <td>Kaloleni Hospital</td>
+                                <td>Arusha</td>
+                                <td>Sanawari</td>
+                                <td>Good</td>
+                                <td>0755688675</td>
+                                <td>Miryam</td>
+                                <td>Doe</td>
+                            </tr>
+
+                            <tr>
+                                <td><a href="new.childinfo.html">151202083539</a></td>
+                                <td>Hagga</td>
+                                <td></td>
+                                <td>Havag</td>
+                                <td>12/3/2000</td>
+                                <td>Male</td>
+                                <td>Baraa RC Dispensary</td>
+                                <td>Arusha</td>
+                                <td>Moshono</td>
+                                <td>Active</td>
+                                <td>0755688675</td>
+                                <td>Miryam</td>
+                                <td>Doe</td>
+                            </tr>
+
+                            <tr>
+                                <td><a href="new.childinfo.html">151209073106</a></td>
+                                <td>John</td>
+                                <td></td>
+                                <td>Hendry</td>
+                                <td>12/3/2000</td>
+                                <td>Male</td>
+                                <td>Kaloleni Hospital</td>
+                                <td>Home</td>
+                                <td>Ngaramtoni</td>
+                                <td>Active</td>
+                                <td>0755688675</td>
+                                <td>Marry</td>
+                                <td>Jones</td>
+                            </tr>
+
+                            <tr>
+                                <td><a href="new.childinfo.html">151207030351</a></td>
+                                <td>John</td>
+                                <td>S</td>
+                                <td>Doe</td>
+                                <td>12/3/2000</td>
+                                <td>Male</td>
+                                <td>Kaloleni Hospital</td>
+                                <td>Arusha</td>
+                                <td>Sanawari</td>
+                                <td>Good</td>
+                                <td>0755688675</td>
+                                <td>Miryam</td>
+                                <td>Doe</td>
+                            </tr>
+
+                            <tr>
+                                <td><a href="new.childinfo.html">151207024748</a></td>
+                                <td>Reina</td>
+                                <td></td>
+                                <td>Mguda</td>
+                                <td>5-Nov-2015</td>
+                                <td>Femaile</td>
+                                <td>Mareu Rural Health Center</td>
+                                <td>Hospital</td>
+                                <td>Moshono</td>
+                                <td>Active</td>
+                                <td>0755688675</td>
+                                <td>Bland</td>
+                                <td>Noah</td>
+                            </tr>
+
+                            <tr>
+                                <td><a  href="new.childinfo.html">151127030222</a></td>
+                                <td>John</td>
+                                <td></td>
+                                <td>Smith</td>
+                                <td>12/3/2000</td>
+                                <td>M</td>
+                                <td>Faith Arusha City Dispensary</td>
+                                <td>Health Facility</td>
+                                <td>Sekei</td>
+                                <td>Active</td>
+                                <td>0755688675</td>
+                                <td>Mary</td>
+                                <td>Smith</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        </div>
  @stop
